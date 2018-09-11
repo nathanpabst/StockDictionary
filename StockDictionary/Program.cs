70c4f1030831e686b4c343280b683859aa38f7f4
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-10 20:34:28 -0500

    purchases list complete

--- StockDictionary/Program.cs
+++ StockDictionary/Program.cs
@@ -12,6 +12,13 @@ namespace StockDictionary
             stocks.Add("CAT", "Caterpillar");
             stocks.Add("AAPL", "Apple");
             stocks.Add("IBM", "IBM");
+
+            List<(string ticker, int shares, double price)> purchases = new List<(string, int, double)>();
+
+            purchases.Add((ticker: "GM", shares: 150, price: 42.24));
+            purchases.Add((ticker: "CAT", shares: 300, price: 56.14));
+            purchases.Add((ticker: "AAPL", shares: 600, price: 942.15));
+            purchases.Add((ticker: "IBM", shares: 500, price: 900.42));
         }
     }
 }
